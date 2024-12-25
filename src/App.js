7202cd2536818d466804f466649977e311614493
Shadestriescoding <tristan.beguet@edu.devinci.fr>
2024-12-25 15:14:39 +0100

    fix: utilisation de HashRouter pour GitHub Pages

--- src/App.js
+++ src/App.js
@@ -2,37 +2,49 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { CartProvider } from './contexts/CartContext';
-import { NotificationProvider } from './contexts/NotificationContext';
+import { ToastProvider } from './contexts/ToastContext';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
-// Pages
+// Layouts
+import Layout from './components/layout/Layout';
+import AdminLayout from './admin/components/AdminLayout';
+
+// Pages publiques
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Contact from './pages/Contact';
+import ProductDetails from './pages/ProductDetails';
 import NotFound from './pages/NotFound';
 
-// Layout
-import Layout from './components/layout/Layout';
+// Pages admin
+import AdminRoutes from './admin/AdminRoutes';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <NotificationProvider>
+      <ToastProvider>
         <CartProvider>
           <GlobalStyle />
           <Router>
             <Routes>
-              <Route element={<Layout />}>
+              {/* Routes publiques */}
+              <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="shop" element={<Shop />} />
+                <Route path="shop/:productId" element={<ProductDetails />} />
                 <Route path="contact" element={<Contact />} />
-                <Route path="*" element={<NotFound />} />
               </Route>
+
+              {/* Routes admin */}
+              <Route path="/admin/*" element={<AdminRoutes />} />
+
+              {/* Route 404 */}
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </Router>
         </CartProvider>
-      </NotificationProvider>
+      </ToastProvider>
     </ThemeProvider>
   );
 };
